e65d1076ffc0176845a21dde95783cf1134b59ca
Tom Allen <tomjamesallenweb@gmail.com>
2015-09-25 11:19:04 +0100

    Working through js errors from http://pareview.sh/pareview/httpgitdrupalorgsandboxtomjamesallen2572263git.

--- js/Imager.useMultipleSrcs.js
+++ js/Imager.useMultipleSrcs.js
@@ -4,6 +4,35 @@
  */
 
 ;(function (window, document) {
+  'use strict';
+
+  // Prefix to look for in dataset.
+  var dataSrcPrefix = 'src-';
+
+  var getIndividualSrc = function (attr, imageParent) {
+    var value = imageParent.dataset[attr];
+
+    // Check whether attribute starts 'src-'.
+    if (attr.indexOf(dataSrcPrefix) !== 0) {
+      return;
+    }
+
+    // Get just the width segment of the data attribute's key.
+    var srcWidth = attr.replace(dataSrcPrefix, '');
+
+    // Return here if we can't get an integer from srcWidth.
+    if (typeof parseInt(srcWidth) !== 'number') {
+      return;
+    }
+
+    // Convert srcWidth to integer.
+    srcWidth = parseInt(srcWidth);
+
+    return {
+      srcWidth: srcWidth,
+      value: value
+    }
+  };
 
   // This is where we need to run the logic to determine the best source.
   var useMultipleSrcs = function (image) {
@@ -27,40 +56,28 @@
     // Array to save available widths to.
     var widths = [];
 
-    // Prefix to look for in dataset.
-    var dataSrcPrefix = 'src-';
+    // Placeholder var to save data to temporarily for each loop.
+    var src;
 
     // We need to cycle through the dataset on the imageParent to find
     // src-[width] attributes.
     for (var attr in imageParent.dataset) {
+      if (!imageParent.dataset.hasOwnProperty(attr)) {
+        continue
+      }
 
-      // Wrapper function.
-      (function (attr, imageParent) {
-        var value = imageParent.dataset[attr];
+      // Get the src data.
+      src = getIndividualSrc(attr, imageParent);
 
-        // Check whether attribute starts 'src-'.
-        if (attr.indexOf(dataSrcPrefix) !== 0) {
-          return;
-        }
+      if (!src) {
+        continue;
+      }
 
-        // Get just the width segment of the data attribute's key.
-        var srcWidth = attr.replace(dataSrcPrefix, '');
+      // Push srcWidth to widths array.
+      widths.push(src.srcWidth);
 
-        // Return here if we can't get an integer from srcWidth.
-        if (typeof parseInt(srcWidth) !== 'number') {
-          return;
-        }
-
-        // Convert srcWidth to integer.
-        srcWidth = parseInt(srcWidth);
-
-        // Push srcWidth to widths array.
-        widths.push(srcWidth);
-
-        // Save image src to srcs, keyed by width.
-        srcs[srcWidth] = value;
-
-      })(attr, imageParent);
+      // Save image src to srcs, keyed by width.
+      srcs[src.srcWidth] = src.value;
     }
 
     // If we don't have any widths then return here.
